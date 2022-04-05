8b3f27af9f94a9f1739d64967a067c82e065a87c
utn203241038 <93666686+utn203241038@users.noreply.github.com>
2022-04-05 01:52:00 -0500

    Update VacantesController.java

--- ver2302/demo (1)/demo/src/main/java/com/gabriel/licona/valdez/demo/controller/VacantesController.java
+++ ver2302/demo (1)/demo/src/main/java/com/gabriel/licona/valdez/demo/controller/VacantesController.java
@@ -34,73 +34,34 @@ import com.gabriel.licona.valdez.demo.util.Utileria;
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
-
-	@Autowired
-	private IntVacantes serviceVacantes;
-	@Autowired
-	private IntServiceCategorias serviceCategoria;
 	
-	@GetMapping("/index")
-	public String mostraIndex(Model model ) {
-		List<Vacante> vacantes = new LinkedList<Vacante>();
-		vacantes = serviceVacantes.obtenerTodos();
-		
-		for(Vacante v : vacantes) {
-			System.out.println(v);
-		}
-		model.addAttribute("total",serviceVacantes.obtenerTodos().size());
-		model.addAttribute("vacantes", vacantes);
-		return "vacantes/listaVacantes";
-		}
+	@Autowired
+	private IntServiceVacantes serviceVacantes;
+	
+	@Autowired
+	private IntServiceCategorias serviceCategorias;
+	
+	@ModelAttribute
+	public void setGenero(Model model) {
+		model.addAttribute("categorias",serviceCategorias.obtenerCategorias());
+		model.addAttribute("vacantes", serviceVacantes.obtenerTodas());	
+	}
+	
 	@GetMapping("/consulta")
 	public String consulta(@RequestParam("id") int idVacante, Model model) {
 		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
-		model.addAttribute("categorias", serviceCategoria.obtenerCategoria());
-		model.addAttribute("vacante", vacante);
-		return "vacantes/formVacantes";
+		model.addAttribute("categorias",serviceCategorias.obtenerCategorias());
+		model.addAttribute("vacante",vacante);
+		return "vacantes/formVacante";
 	}
 	
-	@GetMapping("/eliminar/{id}")
-	public String eliminarVacante(@PathVariable("id") int IdVacante, RedirectAttributes atributo) {
-		atributo.addFlashAttribute("msg","�La categoria se elimino con exito!");
-		//System.out.println("id : " + IdVacante);
-		serviceVacantes.eliminar(IdVacante);
-		return "redirect:/vacantes/indexPaginate";
+	@GetMapping("/mostrar")
+	public String mostrar(@RequestParam("id") Integer id, Model model) {
+		Vacante vac = new  Vacante();
+		vac = serviceVacantes.buscarPorId(id);
+		model.addAttribute("vacante", vac);
+		return "vacantes/detalleVacantes";
 	}
-	
-	@GetMapping("/nueva")
-	public String nuevaCategoria( Vacante vacante, Model model) {
-		model.addAttribute("categorias", serviceCategoria.obtenerCategoria());
-		return "/vacantes/formVacantes";
-		
-	}
-	
-	@GetMapping("/detalle")
-	public String mostradetalle(Model model, @RequestParam("id") Integer IdVacante) {
-		Vacante va = new Vacante();
-		va = serviceVacantes.buscarPorId(IdVacante);
-		model.addAttribute("vacante", va);
-		return "/vacantes/detalle";
-	}
-	
-	/*@PostMapping("/guardar")
-	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion , 
-			@RequestParam("fecha") String fecha ,@RequestParam("salario") Double salario,@RequestParam("estatus") String estatus,
-			@RequestParam("destacado") Integer destacado, @RequestParam("detalle") String detalle) {
-		Vacante va = new Vacante();
-		int id = serviceVacantes.obtenerTodos().size();
-		System.out.println(id);
-		va.setId(++id);
-		va.setNombre(nombre);
-		va.setDescripcion(descripcion);
-		va.setFecha(fecha);
-		va.setSalario(salario);
-		va.setDestacado(destacado);
-		va.setEstatus(estatus);
-		va.setDetalles(detalle);
-		serviceVacantes.agregar(va);
-		return "redirect:/vacantes/index";
-	}*/
 	
 	@PostMapping("/guardar2")
 	public String guardar2(Vacante vacante, BindingResult result, RedirectAttributes attributes, @RequestParam("archivoImagen") MultipartFile multiPart) {
@@ -108,7 +69,7 @@ public class VacantesController {
 			for (ObjectError error: result.getAllErrors()){
 				System.out.println("Ocurrio un error: "+ error.getDefaultMessage());
 				}
-			return "vacantes/formVacantes";
+			return "vacantes/formVacante";
 		}
 		if (!multiPart.isEmpty()) {
 			String ruta = "C:\\empleos\\img-vacantes\\"; // Windows
@@ -116,51 +77,81 @@ public class VacantesController {
 			if (nombreImagen != null){
 			vacante.setImagen(nombreImagen);
 			}}
-		
-		int id = serviceVacantes.obtenerTodos().size();
-		Vacante c = serviceVacantes.obtenerTodos().get(id-1);
-		System.out.println(id);
-		vacante.setId(++id);
-		/*c.setId(++id);
-		c.setNombre(vacante.getNombre());
-		c.setDescripcion(descripcion);
-		cat.setFecha(fecha);
-		cat.setSalario(salario);
-		*/
-		vacante.setCategoria(serviceCategoria.buscarPorId(vacante.getCategoria().getId()));
+		vacante.setCategoria((serviceCategorias.buscarPorId(vacante.getCategoria().getId())));
 		System.out.println(vacante);
 		attributes.addFlashAttribute("msg", "Registro Guardado");
-		serviceVacantes.agregar(vacante);
+		serviceVacantes.guardar(vacante);
 		return "redirect:/vacantes/indexPaginate";
 	}
 	
-	
-	@InitBinder
-	protected void initBinder(WebDataBinder binder) {
-	binder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
-		@Override
-		public void setAsText(String text) throws IllegalArgumentException{
-			setValue(LocalDate.parse(text, DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-		}
+	/*
+	@PostMapping("/guardar")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion, 
+			@RequestParam("categoria") String categoria, @RequestParam("estatus") String estatus, 
+			@RequestParam("fecha") LocalDate fecha, @RequestParam("salario") Double salario, @RequestParam("destacado") Integer destacado,
+			@RequestParam("imagen") String imagen, @RequestParam("detalles") String detalles) {
 		
-		@Override
-		public String getAsText() throws IllegalArgumentException{
-			return DateTimeFormatter.ofPattern("dd-MM-yyyy").format((LocalDate) getValue());
+		Vacante vac = new Vacante();
+		int id = serviceVacantes.obtenerTodas().size();
+		Vacante v = serviceVacantes.obtenerTodas().get(id-1);
+		System.out.println(id);
+		vac.setId(++id);
+		vac.setNombre(nombre);
+		vac.setDescripcion(descripcion);
+		vac.setFecha(fecha);
+		vac.setSalario(salario);
+		vac.setDestacado(destacado);
+		vac.setEstatus(estatus);
+		vac.setImagen(imagen);
+		vac.setDetalles(detalles);
+		serviceVacantes.guardar(vac);
+		return "redirect:/vacantes/index";
+	}*/
+	
+	@GetMapping("/nueva")
+	public String nuevaVacante(Vacante vacante, Model model) {
+		model.addAttribute("categorias", serviceCategorias.obtenerCategorias());
+		return "vacantes/formVacante";
+	}
+	
+	@GetMapping("/eliminar")
+	public String eliminar(@RequestParam("id") int idVacante) {
+		serviceVacantes.eliminar(idVacante);
+		return "redirect:/vacantes/indexPaginate";
+	}
+	
+	@GetMapping("/index")
+	public String mostrarIndex(Model model) {
+		List<Vacante> vacantes = new LinkedList<Vacante>();
+		vacantes = serviceVacantes.obtenerTodas();
+		for(Vacante v : vacantes) {
+			//System.out.println(v);
 		}
-	});
-}
-	@ModelAttribute
-	public void setGenerico(Model model) {
-		model.addAttribute("categorias", serviceCategoria.obtenerCategoria());
-		model.addAttribute("vacantes", serviceVacantes.obtenerTodos());
+		//model.addAttribute("vacantes", vacantes);
+		model.addAttribute("total", serviceVacantes.obtenerTodas().size());
+		return "vacantes/listaVacantes";
 	}
 	
 	@GetMapping(value = "/indexPaginate")
 	public String mostrarIndexPaginado(Model model, Pageable page) {
-	Page<Vacante> lista = serviceVacantes.buscarTodas(page);
-	model.addAttribute("total", serviceVacantes.buscarTodas(page));
-	model.addAttribute("vacantes", lista);
-	return "/vacantes/listaVacantes";
+		Page<Vacante> lista = serviceVacantes.buscarTodas(page);
+		model.addAttribute("total", serviceVacantes.numeroRegistros());
+		model.addAttribute("vacantes", lista);
+		return "vacantes/listaVacantes";
 	}
 	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) throws IllegalArgumentException{
+				setValue(LocalDate.parse(text, DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+			}
+			
+			@Override
+			public String getAsText() throws IllegalArgumentException {
+				return DateTimeFormatter.ofPattern("dd-MM-yyyy").format((LocalDate) getValue());
+			}
+		});
+	}
 }	
